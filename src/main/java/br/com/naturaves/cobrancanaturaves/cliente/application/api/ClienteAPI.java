41ad79cb4ac6569baa8e3219d6622fdb48f71f86
Danilo1dev <danilofoxlog@gmail.com>
2022-07-31 20:36:17 -0300

    implementa controller

--- src/main/java/br/com/naturaves/cobrancanaturaves/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/naturaves/cobrancanaturaves/cliente/application/api/ClienteAPI.java
@@ -10,8 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
-	
-	
+
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	ClienteResponse postCliente(@RequestBody clienteRequest clienteRequest);
