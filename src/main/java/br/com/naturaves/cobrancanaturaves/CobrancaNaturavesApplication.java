eab9d9cdc6d1f2e88b70c76224f8afd9a9dffb8f
Danilo1dev <danilofoxlog@gmail.com>
2022-07-31 18:07:38 -0300

    inicia projeto

---
+++ src/main/java/br/com/naturaves/cobrancanaturaves/CobrancaNaturavesApplication.java
@@ -0,0 +1,13 @@
+package br.com.naturaves.cobrancanaturaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancaNaturavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancaNaturavesApplication.class, args);
+	}
+
+}
