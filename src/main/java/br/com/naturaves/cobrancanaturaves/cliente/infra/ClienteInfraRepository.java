b6ee8aa6343a2477ff1b1788bc61bc14b4b758d5
Danilo1dev <danilofoxlog@gmail.com>
2022-10-12 22:13:29 -0300

    refactor tratamento de exception em package cliente

--- src/main/java/br/com/naturaves/cobrancanaturaves/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/naturaves/cobrancanaturaves/cliente/infra/ClienteInfraRepository.java
@@ -2,9 +2,12 @@ package br.com.naturaves.cobrancanaturaves.cliente.infra;
 
 import java.util.List;
 import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import br.com.naturaves.cobrancanaturaves.cliente.application.repository.ClienteRepository;
 import br.com.naturaves.cobrancanaturaves.cliente.domain.Cliente;
+import br.com.naturaves.cobrancanaturaves.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -34,7 +37,7 @@ public class ClienteInfraRepository implements ClienteRepository {
 	public Cliente buscaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia]ClienteInfraRepository - buscaClienteAtravesID");
 		Cliente cliente = clienteSpringDataJPARepository.findById(idCliente)
-				.orElseThrow(() -> new RuntimeException("Cliente não encontrado!"));
+				.orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND, "Cliente não encontrado pelo idCliente " + idCliente));
 		log.info("[finaliza]ClienteInfraRepository - buscaClienteAtravesID");
 		return cliente;
 	}
