e9daa0636251e46af39de8351cd052993c4d065a
Danilo1dev <danilofoxlog@gmail.com>
2022-10-15 23:19:47 -0300

    teste FindByIDNotFound

--- src/test/java/br/com/naturaves/cobrancanaturaves/service/ClienteServiceTest.java
+++ src/test/java/br/com/naturaves/cobrancanaturaves/service/ClienteServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.naturaves.cobrancanaturaves.service;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import java.util.UUID;
@@ -11,8 +13,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteDetalhadoResponse;
 import br.com.naturaves.cobrancanaturaves.cliente.application.api.ClienteResponse;
-import br.com.naturaves.cobrancanaturaves.cliente.application.repository.ClienteRepository;
 import br.com.naturaves.cobrancanaturaves.cliente.application.service.ClienteApplicationService;
+import br.com.naturaves.cobrancanaturaves.cliente.infra.ClienteInfraRepository;
 import br.com.naturaves.cobrancanaturaves.mock.MockCliente;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,7 +23,7 @@ public class ClienteServiceTest {
 	@InjectMocks
 	private ClienteApplicationService clienteService;
 	@Mock
-	private ClienteRepository clienteRepository;
+    private ClienteInfraRepository clienteRepository;
 
 	@Test
 	void testCreateClientSucess() {
@@ -36,4 +38,15 @@ public class ClienteServiceTest {
 		ClienteDetalhadoResponse clienteResponse = clienteService.buscaClienteAtravesID(UUID.randomUUID());
 		assertNotNull(clienteResponse);
 	}
+
+	@Test
+	void testFindByIDNotFound() {
+		when (clienteRepository.buscaClienteAtravesId(any())).thenThrow(new RuntimeException("Cliente não encontrada!"));
+		RuntimeException exception = assertThrows(RuntimeException.class, () -> clienteService.buscaClienteAtravesID(UUID.randomUUID()));
+		assertNotNull(exception);
+	     assertEquals("Cliente não encontrada!", exception.getMessage());
+	}
 }
+
+
+
